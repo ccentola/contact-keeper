195075e16928ad439560f400b436930d185a37df
Carl Centola <carl.centola@gmail.com>
2021-11-22 11:27:16 -0700

    added mongodb to project

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the Contact Keeper API" })
+// connect database
+connectDB();
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the Contact Keeper API' })
 );
 // define routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 3001;
 
